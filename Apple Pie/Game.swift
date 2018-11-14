e839d69f4929339c5a92e52dd558db408357f12d
JARBerry <rayberry@rays-macbook.local>
2018-11-14 15:52:12 +0000

    Created Game.swift as model. Linked up outlets and actions to letters. Added wins and losses display

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Ray Berry on 14/11/2018.
+//  Copyright © 2018 JARBerry. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -2,18 +2,62 @@
 //  ViewController.swift
 //  Apple Pie
 //
-//  Created by Ray Berry on 14/11/2018.
+//  Created by Ray and James Berry on 14/11/2018.
 //  Copyright © 2018 JARBerry. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var listOfWords = ["dog", "cat", "monkey", "lion", "kangaroo", "leopard", "elephant", "giraffe", "rhino", "chimpanzee"]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0
+    
+    var totalLosses = 0
+    
+    var currentGame: Game!
+    
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       newRound()
     }
+    
+    
+   
+    
+    func newRound() {
+        
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+   sender.isEnabled = false
+        
+        
+        
+        
+    
+    }
+    
 
 
 }
