b043cfd1ea434f1ae12db6916b0bf9b55028e5f8
JARBerry <rayberry@rays-macbook.local>
2018-11-15 10:57:32 +0000

    Game updated to enable buttons after each word guessed

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -14,11 +14,21 @@ class ViewController: UIViewController {
     
     let incorrectMovesAllowed = 7
     
-    var totalWins = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
     
-    var totalLosses = 0
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     
     var currentGame: Game!
+    
+    
     
     
     @IBOutlet weak var treeImageView: UIImageView!
@@ -41,10 +51,21 @@ class ViewController: UIViewController {
     
     func newRound() {
         
+        if !listOfWords.isEmpty {
         let newWord = listOfWords.removeFirst()
         currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
         updateUI()
-        
+        } else {
+            enableLetterButtons(false)
+            
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
     }
     
     // update total scores
@@ -65,11 +86,21 @@ class ViewController: UIViewController {
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
-        updateUI()
+        updateGameState()
         
     }
     
-
+    func updateGameState() {
+        
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+        
+    }
 
 }
 
