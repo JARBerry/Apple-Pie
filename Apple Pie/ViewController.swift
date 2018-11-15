7674f000ca0131eb562c5aee85f2c412e4f1a4e2
JARBerry <rayberry@rays-macbook.local>
2018-11-15 09:59:22 +0000

    Added functionality to guess letters and to update images when wrong

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -34,28 +34,39 @@ class ViewController: UIViewController {
     }
     
     
+    
+    
    
+    //New round - retrieve new word and reset incorrect moves
     
     func newRound() {
         
         let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
         updateUI()
         
     }
     
+    // update total scores
+    
     func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
    sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateUI()
         
-        
-        
-        
-    
     }
     
 
